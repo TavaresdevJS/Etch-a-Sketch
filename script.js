b9a01c97170b6d28f78a5f13899a8239a7a66682
TavaresdevJS <178415368+TavaresdevJS@users.noreply.github.com>
2026-07-20 11:54:59 -0300

    Add choose grid size button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 const grid = document.querySelector('.grid');
+const sizeBtn = document.querySelector('.sizeBtn');
+const sizeInput = document.querySelector('.input');
 
 function createGrid(size) {
   grid.innerHTML = '';
@@ -10,6 +12,10 @@ function createGrid(size) {
     square.style.opacity = '0';
     square.addEventListener('mouseover', function () {
       let currentOpacity = parseFloat(square.style.opacity);
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+      square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
       if (currentOpacity < 1.0) {
         square.style.opacity = (currentOpacity + 0.1).toString();
       }
@@ -18,4 +24,11 @@ function createGrid(size) {
   }
 }
 
-createGrid(16);
+sizeBtn.addEventListener('click', function () {
+  const inputText = +sizeInput.value;
+  if (!inputText || inputText > 100 || inputText < 1) {
+    alert('Wrong input, try a number from 1 to 16');
+  } else {
+    createGrid(inputText);
+  }
+});
