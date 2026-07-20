d76f17c2221c4d113526778f0aca9fe1a65a693c
TavaresdevJS <178415368+TavaresdevJS@users.noreply.github.com>
2026-07-20 11:21:02 -0300

    Add progressive darkening effect on mouse hover

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ function createGrid(size) {
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
+    square.style.opacity = '0';
+    square.addEventListener('mouseover', function () {
+      let currentOpacity = parseFloat(square.style.opacity);
+      if (currentOpacity < 1.0) {
+        square.style.opacity = (currentOpacity + 0.1).toString();
+      }
+    });
     grid.appendChild(square);
   }
 }
+
+createGrid(16);
